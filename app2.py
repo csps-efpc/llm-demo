8cf3dae7a32baaba69d3e11198a5d85cef75ab87
Handshape <jturner@CSPS002478.csps-efpc.com>
2023-09-19 12:01:51 -0400

    Add version with optimized support for GPUs

---
+++ app2.py
@@ -0,0 +1,107 @@
+# We import llama.cpp - an optimized LLM inference runtime, as well as flask (a tiny app 
+# server), and a websockets library for flask 
+from llama_cpp import Llama
+from llama_cpp import ChatCompletionMessage
+from flask import request, redirect
+from flask import render_template
+from flask_sock import Sock
+import flask
+ 
+# json and sys are imported from core libraries
+import json
+import sys
+ 
+# Your model will interact according to two key patterns: the system prompt and the user 
+# prompt. The former describes the "terms of engagement" that your model will conform to.
+# The latter is a fixed format that feeds the user's interaction to the model once for each
+# prompt. Both of these will be declared on the model card for the model you choose.
+# An interesting consideration is that the model "reads" the system prompt before starting 
+# each conversation, at the same speed as it generates tokens. A longer system prompt means 
+# a longer startup time for each conversation. 
+system_prompt="""A chat.
+"""
+ 
+# We'll replace the {0} value with the user's input.
+template = """USER: {0}
+ASSISTANT: 
+"""
+ 
+# Selecting how many threads is important - during inference, the process *will* soak the 
+# affected cores at 100%. Never allocate more threads than you have physical hardware.
+ 
+# Declaration of the flask instance
+app = flask.Flask(__name__)
+ 
+# Add the websocket support to the flask server
+sock = Sock(app)
+ 
+# Add a redirect that send users to the index page.
+@app.route('/')
+def root():
+    return redirect("/index.html", code=302)
+ 
+# Add an endpoint that serves the single-file application found in templates/index.html
+@app.route('/index.html', methods=['GET'])
+def index_html():
+    return render_template('index.html')
+ 
+ 
+# Here's the meat of the interaction: a websockets endpoint that runs a single conversation with the
+# LLM during its life.
+@sock.route('/gpt-socket')
+def gpt_socket(ws):
+    # We instantiate a chat session and attach our tempaltes to it, and loop until the socket ends.
+# We start by loading and instantiating the LLM model. Choosing your model is a big deal,
+# and should be done carefully. Look particularly carefully at the licenses, and any 
+# restrictions they may place on your use of the model. https://gpt4all.io/ is a good 
+# place to start looking. https://huggingface.co/TheBloke is another. At the time of
+# writing, llama.cpp works best with GGUFv3 models quantized down to about 4-5 bits, but will
+# work (slowly) with finer models up to the q8 level.
+    chat_session = ""
+    chat_session += system_prompt
+
+    #llm = Llama(model_path="./airoboros-l2-7b-gpt4-m2.0-gguf.bin", n_gpu_layers=-1, n_threads=4, n_ctx=2048)
+    llm = Llama(model_path="./airoboros-l2-7b-2.1.Q4_K_M.gguf", n_gpu_layers=-1, n_threads=4, n_ctx=2048)
+    while True:
+        # We receive and log the user prompt.
+        message = ws.receive()
+        print("WebSocket prompt: " + message)
+        # The model will respond with a series of tokens - words, characters, and word-fragments 
+        # expressed as strings. We accumulate the tokens in outList so we can add them to the 
+        # conversation when the model finishes emitting them. 
+ 
+        # Here's where we ask the model to start generating tokens. The call to generate()
+        # emits a Python generator over which we iterate. The n_predict parameter is the cap
+        # on the number of tokens we want the model to emit.
+        chat_session += template.format(message)
+        stream = llm(
+            chat_session,
+            max_tokens=2048,
+            stop=["USER:"],
+            stream=True
+        )
+        for tok in stream:
+            text = tok["choices"][0]["text"]
+            # First we accumulate the token in outList, then we send it as a websocket message to
+            # the client.
+            chat_session += text
+            ws.send( text )
+            print(text, end='')
+        # We send a specific websocket message to the client to indicate that the model has 
+        # finished generating.
+        ws.send('<END>')
+        # We append the text that we received from the model to the conversation session. 
+ 
+# Here's an example of an HTTP (rather than websocket) endpoint. Much simpler. Note that because 
+# HTTP is stateless, the "conversation" ends after every response. 
+# @app.route('/gpt', methods=['GET', 'POST'])
+# def stream():
+#     prompt = request.args['prompt']
+#     with model.chat_session(prompt_template = template, system_prompt = system_prompt):
+#         return flask.Response( model.generate(prompt, n_predict=2048 ), mimetype='text/plain')
+ 
+# Here's where we start the app, listening on all interfaces. Flask defaults to listening on port 5000.
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+ 
+#debug=True
